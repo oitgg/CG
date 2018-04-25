6730cdac13c5f494c532eeae65740081cca5fc57
Tgg <37004066+oitgg@users.noreply.github.com>
2018-04-24 21:29:30 -0300

    CML Update

---
+++ TF-PortalGL/CML/cml/version.h
@@ -0,0 +1,21 @@
+/* -*- C++ -*- ------------------------------------------------------------
+ @@COPYRIGHT@@
+ *-----------------------------------------------------------------------*/
+/** @file
+ */
+
+#pragma once
+
+#ifndef	cml_version_h
+#define	cml_version_h
+
+/* Current CML version: */
+#define CML_VERSION			200101U
+
+/* Current CML version as a string: */
+#define CML_VERSION_STRING		"2.1.1"
+
+#endif
+
+// -------------------------------------------------------------------------
+// vim:ft=cpp:sw=2
